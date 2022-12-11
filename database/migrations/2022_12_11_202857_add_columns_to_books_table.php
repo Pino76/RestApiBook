8e17d795f05bc1f76aa6beb34c051d247eaff6d5
pinof <fattoruso.g@gmail.com>
2022-12-11 21:59:35 +0100

    fix: added new fields

---
+++ database/migrations/2022_12_11_202857_add_columns_to_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->foreignId('user_id')->after('id')->constrained()->onUpdate('cascade');
+            $table->year('year_of')->after('isbn');
+            $table->softDeletes()->after('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('year_of');
+            $table->dropSoftDeletes();
+        });
+    }
+};
